c0121b70328f49c52df16fef165194a2bb836f52
btlaube <69732464+btlaube@users.noreply.github.com>
2024-02-14 22:36:08 -0600

    initial commit and onlune 3d unity template

---
+++ Online_3D/Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerNetwork : NetworkBehaviour {
+    
+    [SerializeField] private Transform spawnedObjectPrefab;
+    private Transform spawnedObjectTransform;
+
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData {
+            _int = 56,
+            _bool = true,
+        }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public struct MyCustomData : INetworkSerializable {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
+
+    public override void OnNetworkSpawn() {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) => {
+            Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
+        };
+    }
+
+    private void Update() {
+
+        if (!IsOwner) return;
+
+        if (Input.GetKeyDown(KeyCode.T)) {
+            spawnedObjectTransform = Instantiate(spawnedObjectPrefab);
+            spawnedObjectTransform.GetComponent<NetworkObject>().Spawn(true);
+            // TestClientRpc(new ClientRpcParams { Send = new ClientRpcSendParams { TargetClientIds = new List<ulong> { 1 } } } );
+            // randomNumber.Value = new MyCustomData {
+            //     _int = Random.Range(0, 100),
+            //     _bool = Random.value > 0.5,
+            //     message = "Fard. And also shid."
+            // };
+        }
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Destroy(spawnedObjectTransform.gameObject);
+        }
+
+        Vector3 moveDir = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W)) moveDir.z += 1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x -= 1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z -= 1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x += 1f;
+
+        float moveSpeed = 3f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+
+    [ServerRpc]
+    private void TestServerRpc(ServerRpcParams serverRpcParams) {
+        Debug.Log("TestServerRpc " + OwnerClientId + "; " + serverRpcParams.Receive.SenderClientId);
+    }
+
+    [ClientRpc]
+    private void TestClientRpc(ClientRpcParams clientRpcParams) {
+        Debug.Log("testClientRpc");
+    }
+
+}
